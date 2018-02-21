94782d78bc58b09cdc423821e6f59a21978bc522
jordanvogel <jvogel@ualberta.ca>
2018-02-20 21:49:38 -0700

    first layout commit of the monitor screen

--- src/screens/MonitorScreen.js
+++ src/screens/MonitorScreen.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, Dimensions, TextInput } from 'react-native';
 import {API_KEYS} from '../api'
 
 import { NearbyAPI } from "react-native-nearby-api";
 
-
-
 // const nearbyAPI = new NearbyAPI(true);
+
+var {height, width} = Dimensions.get('window');
 
 export default class MonitorScreen extends Component {
   static navigationOptions = {
@@ -15,8 +15,10 @@ export default class MonitorScreen extends Component {
   constructor() {
     super();
     this.state = {
-      message: ""
+      message: "",
+      temp: "98"
     };
+
     // nearbyAPI.connect(API_KEYS.nearby);
     // console.log(API_KEYS.nearby);
     
@@ -34,9 +36,23 @@ export default class MonitorScreen extends Component {
 
   render() {
     return (
-      <Text>
-        M:{this.state.message}
-      </Text>
-    )
+      // Try setting `flexDirection` to `column`.
+      <View style={{flex:1, flexDirection: 'column'}}>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{width: width*0.7, height: height*0.2, backgroundColor: 'powderblue'}} />
+          <Text style = {{width: width*0.3, height: height*0.2, fontSize: 60}}>
+          <Text>{this.state.temp}</Text>
+          </Text>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{width: width*0.7, height: height*0.33, backgroundColor: 'powderblue'}} />
+          <View style={{width: width*0.3, height: height*0.33, backgroundColor: 'steelblue'}} />
+        </View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{width: width*0.7, height: height*0.33, backgroundColor: 'powderblue'}} />
+          <View style={{width: width*0.3, height: height*0.33, backgroundColor: 'steelblue'}} />
+        </View>
+      </View>
+    );
   }
-}
+};
