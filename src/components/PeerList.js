9056f2856094184f3e46dcb873612ad5d954e4e8
Braedy Kuzma <braedy@ualberta.ca>
2018-02-25 18:57:04 -0700

    Better formatting in list.
    The image formatting was actually terrible. After finding some javascript tips
    online this seems too look better and also formats better with long names. See
    comments in code.

--- src/components/PeerList.js
+++ src/components/PeerList.js
@@ -12,8 +12,11 @@ class PeerListElement extends PureComponent {
     const item = this.props.item;
     return (
       <View style={styles.listElement}>
-        <Text style={{fontSize: 25}}>{item.id}</Text>
-        <Image source={require('./img/wifi.png')}/>
+        <Text style={styles.connectionName}>{item.id}</Text>
+          <Image
+            source={require('./img/wifi.png')}
+            style={styles.connectionType}
+          />
       </View>
     )
   }
@@ -41,5 +44,17 @@ const styles = StyleSheet.create({
   listElement: {
     flexDirection: 'row',
     justifyContent: 'space-between'
+  },
+  connectionName: {
+    flex: 0.7,
+    fontSize: 25
+  },
+  connectionType: {
+    flex: 0.3,
+    alignSelf: 'center', // Align image vertically center, handles list elements
+                         // with names longer than a single line vertically
+    width: undefined,
+    // height: undefined,
+    resizeMode: 'contain'
   }
 })
