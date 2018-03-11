5e4f43e5b36a8c86ff38b65d927d59bcbe8a44de
hughboi <hdc@ualberta.ca>
2018-03-10 17:48:24 -0700

    fix landscape issues

--- src/screens/SelectModeScreen.js
+++ src/screens/SelectModeScreen.js
@@ -5,20 +5,13 @@ import {
   StyleSheet,
   StatusBar,
   Alert,
-  Platform,
-  Dimensions
+  Platform
 } from 'react-native';
 import { Container, Content, Button, Text, Grid, Row, Col } from 'native-base';
 import PeerList from '../components/PeerList';
 import styles from "./styles/selectModeScreenStyle";
 
 export default class SelectModeScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      windowHeight: 0,
-    };
-  }
   componentDidMount(){
     StatusBar.setHidden(false);
   }
@@ -27,31 +20,26 @@ export default class SelectModeScreen extends Component {
     title: 'Select Mode'
   }
 
-  find_height(layout){
-    // Was having troubles getting height, so this work around needed
-    // https://stackoverflow.com/questions/30203154/get-size-of-a-view-in-react-native/41398953#41398953
-    // Date Taken: March 9, 2018
-    const {x, y, width, height} = layout;
-    this.setState({windowHeight: height});
-  }
-
   render() {
     const {navigate} = this.props.navigation;
     return (
       <Container>
-        <View style={{height: '100%'}}>
+        <View style={{flex: 1, height: '100%'}}>
           <Grid>
             <Col>
             </Col>
             <Col size={6}>
-              <Row size={1.5} style={{alignSelf: 'center'}}>
+              <Row size={1} style={{alignSelf: 'center'}}>
                 <Text style={{fontSize: 50, color: '#8c8f93', marginTop: 10}}> Mock Code </Text>
               </Row>
-              <Row size={0.5}>
+              <Row size={0.6}>
+                <Col>
                 <Text style={{fontSize: 30, color: '#8c8f93'}}> Choose Interface </Text>
+                <View style={{ borderBottomWidth: 1.5, borderBottomColor: '#c2c4c6', width: '95%', alignSelf: 'center'}}/>
+                </Col>
               </Row>
               <Row size={0.1} style={{alignSelf: 'center'}}>
-                <View style={{ borderBottomWidth: 2, borderBottomColor: '#c2c4c6', width: '95%', marginBottom: 10}}/>
+                
               </Row>
               <Row size={0.8} style={{alignSelf: 'center'}}>
                 <Button style={{width: '50%'}} full onPress={() => navigate('ControllerScreen')}>
@@ -68,11 +56,11 @@ export default class SelectModeScreen extends Component {
                   <Text> Patient Face </Text>
                 </Button>
               </Row>
-              <Row size={0.5}>
-                <Text style={{fontSize: 30, color: '#8c8f93'}}> Nearby </Text>                         
-              </Row>
-              <Row size={0.05} style={{alignSelf: 'center'}}>
-                <View style={{ borderBottomWidth: 2, borderBottomColor: '#c2c4c6', width: '95%'}}/>
+              <Row size={0.6}>
+                <Col>
+                  <Text style={{fontSize: 30, color: '#8c8f93'}}> Nearby </Text>
+                  <View style={{ borderBottomWidth: 1, borderBottomColor: '#c2c4c6', width: '95%', alignSelf: 'center'}}/>                                         
+                </Col>
               </Row>
               <Row size={5} style={{alignSelf: 'center', paddingHorizontal: '10%'}}>
                 <PeerList data={[{id: '1'}, {id: '2'}, {id: '3'}, {id: '4'}, {id: '5'}, {id: '6'}, {id: '7'}, {id: '8'}, {id: '9'}, {id: '10'}, {id: '11'}, {id: '12'} ]}/>
