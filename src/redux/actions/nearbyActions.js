29083ef80947ab04cc7baf3ceae0d2993b510fab
Braedy Kuzma <braedy@ualberta.ca>
2018-03-11 21:01:13 -0600

    Fix tabbing.

--- src/redux/actions/nearbyActions.js
+++ src/redux/actions/nearbyActions.js
@@ -8,8 +8,8 @@ export const ACTIONS = {
     UPDATE_BLOOD_PRESSURE: "UPDATE_BLOOD_PRESSURE",
     UPDATE_O2SAT: "UPDATE_O2_SAT",
     UPDATE_ETCO2: "UPDATE_ET_CO2",
-		MESSAGE_FOUND: "ON_MESSAGE_FOUND",
-		UPDATE_FACE: "UPDATE_FACE"
+    MESSAGE_FOUND: "ON_MESSAGE_FOUND",
+    UPDATE_FACE: "UPDATE_FACE"
 }
 
 export const ControllerValues = {
@@ -18,18 +18,18 @@ export const ControllerValues = {
 
 
 export function Update_Store(slider, value) {
-	console.log("test")
-	console.log(slider)
+  console.log("test")
+  console.log(slider)
 
   return { type: slider, value:value}
 }
 export function Update_Value(type, value) {
-	return (dispatch, getState) => {
-		// var state = getState();
-		let m = { message: value, type: type };
-		getState().NearbyApi.nearbyApi.publish(JSON.stringify(m))
-		return dispatch(Update_Store(type, value));
-	}
+  return (dispatch, getState) => {
+    // var state = getState();
+    let m = { message: value, type: type };
+    getState().NearbyApi.nearbyApi.publish(JSON.stringify(m))
+    return dispatch(Update_Store(type, value));
+  }
 
 }
 
@@ -38,19 +38,19 @@ export function Update_Value(type, value) {
 
 export function On_Message_Found(message) {
   return (dispatch) => {
-		let m = JSON.parse(message);
-		dispatch(Update_Store(m.type, m.message)) 
-		return dispatch({ type: ACTIONS.MESSAGE_FOUND, value: message })
-	} 
+    let m = JSON.parse(message);
+    dispatch(Update_Store(m.type, m.message))
+    return dispatch({ type: ACTIONS.MESSAGE_FOUND, value: message })
+  }
 }
 
 export function On_Message(message) {
-	return { type: ACTIONS.MESSAGE_FOUND, value: message }
+  return { type: ACTIONS.MESSAGE_FOUND, value: message }
 }
 
 // export function Network_Connect_and_listen(value) {
 //   return (dispatch, getState) => {
-// 		return getstate().nearbyAPI.connect(API_KEYS.nearby)
+//     return getstate().nearbyAPI.connect(API_KEYS.nearby)
 //   }
-  
+
 // }
