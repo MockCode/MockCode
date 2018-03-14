e251e0af26898a31756f6a49a37a7adf683b0b04
Braedy Kuzma <braedy@ualberta.ca>
2018-03-13 22:32:29 -0600

    Add new error checking.

--- src/components/FaceButton.js
+++ src/components/FaceButton.js
@@ -6,16 +6,23 @@ import PropTypes from 'prop-types';
 import { Update_Value, ACTIONS } from '../redux/actions/nearbyActions';
 import { faceNames } from '../screens/PatientScreen';
 
-
 export default class FaceButton extends PureComponent {
-
   // Validate props
   static propTypes = {
-    // We need to make sure that the value prop is actually a face name
+    // We need to make sure that the value prop is actually a string and a face
+    // name
     value: function(props, propName, componentName) {
       let key = props[propName];
+      let type = typeof key;
+
+      if (type === "undefined")
+        return new Error('Value prop is required');
+
+      if (!(type === "string"))
+        return new Error('Invalid value prop type: ' + type);
+
       if (!(key in faceNames))
-        return new Error('Invalid value prop in FaceButton: ' + key);
+        return new Error('Value prop is not a patient face name: ' + key);
     },
   }
 
@@ -23,7 +30,7 @@ export default class FaceButton extends PureComponent {
     const {value} = this.props;
     store.dispatch(Update_Value(ACTIONS.UPDATE_FACE, value));
   }
-  
+
   render() {
     const {value} = this.props;
     return (
