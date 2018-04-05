0a05c46614b70949922de0d7143a8360af6a5722
jordanvogel <jordan.n.vogel@gmail.com>
2018-04-05 17:22:46 -0600

    Fixing the devastating android version bug

--- android/app/src/main/java/com/mockcode/MainApplication.java
+++ android/app/src/main/java/com/mockcode/MainApplication.java
@@ -3,25 +3,12 @@ package com.mockcode;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import fr.greweb.rnwebgl.RNWebGLPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.horcrux.svg.SvgPackage;
 import com.github.yamill.orientation.OrientationPackage;
-import com.badfeatures.nearby.RNNearbyApiPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 
-import fr.greweb.rnwebgl.RNWebGLPackage;
-import com.brentvatne.react.ReactVideoPackage;
-import com.horcrux.svg.SvgPackage;
-import com.github.yamill.orientation.OrientationPackage;
 import com.badfeatures.nearby.RNNearbyApiPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-
-import fr.greweb.rnwebgl.RNWebGLPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.brentvatne.react.ReactVideoPackage;
-import com.badfeatures.nearby.RNNearbyApiPackage;
-import com.github.yamill.orientation.OrientationPackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
@@ -45,26 +32,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNWebGLPackage(),
             new ReactVideoPackage(),
             new SvgPackage(),
-            new OrientationPackage(),
-            new RNNearbyApiPackage(),
             new RNDeviceInfo(),
-
-
-
-            new RNWebGLPackage(),
-            new ReactVideoPackage(),
-            new SvgPackage(),
-            new OrientationPackage(),
-            new RNNearbyApiPackage(),
-            new RNDeviceInfo(),
-            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
-            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
-            new RNWebGLPackage(),
-            new RNDeviceInfo(),
-            new ReactVideoPackage(),
             new RNNearbyApiPackage(),
             new OrientationPackage(),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appcenterCrashes_whenToSendCrashes)),
