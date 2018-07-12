22305d15e9e27c822b71ce82bb456d4d94b583e2
hughboi <hdc@ualberta.ca>
2018-07-12 14:41:23 -0600

    added AppState checks to network comp

--- src/components/network.js
+++ src/components/network.js
@@ -1,49 +1,90 @@
-import React, { Component } from 'react'
-import {Platform} from 'react-native'
+import React from 'react'
+import {Platform, AppState} from 'react-native'
 import {API_KEYS} from '../api'
 import {On_Message_Found, ACTIONS} from '../redux/actions/nearbyActions'
 import DeviceInfo from 'react-native-device-info'
 
-
-export class NetworkComp extends Component {
-  componentDidMount() {
-    state = store.getState()
-    var nearbyApi = state.NearbyApi.nearbyApi;
-    if (nearbyApi != undefined) {
-
-      if (Platform.OS != 'Jest') {
-        nearbyApi.connect(API_KEYS.nearby)
-      }
-  
-      nearbyApi.onConnected(message => {
-        //TODO: dispatch successful connect action
-        console.log("Connected to Nearby.");
-        nearbyApi.subscribe();
-      });
-  
-      nearbyApi.onSubscribeSuccess(() => {
-        let m = {type:ACTIONS.HELLO_REQUEST, message: DeviceInfo.getDeviceName()}
-        nearbyApi.publish(JSON.stringify(m));
-        // console.log(m)
-      })
-      nearbyApi.onPublishSuccess(message => {
-        // console.log(message, "psucess");
-      });
-      nearbyApi.onFound(message => {
-        console.log("GOTCHA!", message);
-        store.dispatch(On_Message_Found(message));
-      });
+export class NetworkComp extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            appState: AppState.currentState
+        }
     }
-  };
-  
-  componentWillUnmount() {
-    console.log("network is about to be destroyed");
-    // TODO: unsubscribe and disconnect
-  };
 
-  render() {
-    return (
-      null
-    )
-  }
+    _handleAppStateChange = (nextAppState) => {
+        let nearbyApi = store.getState().NearbyApi.nearbyApi;
+        if(this.state.appState.match(/active/)
+            && (nextAppState === 'inactive'|| nextAppState === 'background')) {
+            console.log("unsubscribing.");
+            nearbyApi.unpublish();
+            nearbyApi.unsubscribe();
+        } else {
+            console.log("resubscribing.");
+            nearbyApi.subscribe();
+        }
+    }
+
+    componentDidMount() {
+        state = store.getState()
+        var nearbyApi = state.NearbyApi.nearbyApi;
+        if (nearbyApi != undefined) {
+
+            if (Platform.OS != 'Jest') {
+                nearbyApi.connect(API_KEYS.nearby)
+            }
+
+            nearbyApi.onConnected(message => {
+                //TODO: dispatch successful connect action
+                console.log("Connected to Nearby.");
+                nearbyApi.subscribe();
+            });
+
+            nearbyApi.onSubscribeSuccess(() => {
+                let m = {
+                    type:ACTIONS.HELLO_REQUEST,
+                    message: DeviceInfo.getDeviceName()
+                }
+                nearbyApi.publish(JSON.stringify(m));
+                // console.log(m)
+            })
+            nearbyApi.onPublishSuccess(message => {
+                console.log(message, "psucess");
+            });
+
+            nearbyApi.onPublishFailed(message => {
+                console.log("FAILED TO SEND PAYLOAD.");
+            });
+
+            nearbyApi.onFound(message => {
+                console.log("GOTCHA!", message);
+                store.dispatch(On_Message_Found(message));
+            });
+
+            nearbyApi.onLost(message => {
+                console.log("LOST PAYLOAD! RESEND PLS!", message);
+            });
+
+            nearbyApi.onSubscribeFailed(() => {
+                console.log("SUBSCRIBE FAILED!");
+            });
+
+            nearbyApi.onDisconnected(message => {
+                console.log("NEARBY API DISCONNECTED.");
+            });
+        }
+        AppState.addEventListener('change', this._handleAppStateChange);
+    };
+
+    componentWillUnmount() {
+        AppState.removeEventListener('change', this._handleAppStateChange);
+        console.log("network is about to be destroyed");
+        // TODO: unsubscribe and disconnect
+    };
+
+    render() {
+        return (
+            null
+        )
+    }
 }
--- src/redux/actions/nearbyActions.js
+++ src/redux/actions/nearbyActions.js
@@ -1,14 +1,11 @@
 import DeviceInfo from 'react-native-device-info'
 
-export const UPDATE_SLIDER = 'UPDATE_SLIDER';
-
 export const ACTIONS = {
     UPDATE_HEART_RATE: "UPDATE_HEART_RATE",
     UPDATE_BLOOD_PRESSURE: "UPDATE_BLOOD_PRESSURE",
     UPDATE_O2SAT: "UPDATE_O2_SAT",
 	  UPDATE_ETCO2: "UPDATE_ET_CO2",
 	  UPDATE_WAVEFORM: "UPDATE_WAVEFORM",
-    MESSAGE_FOUND: "ON_MESSAGE_FOUND",
 		HELLO_RESPONSE: "HELLO_RESPONSE",
 		HELLO_REQUEST: "HELLO_REQUEST",
     UPDATE_FACE: "UPDATE_FACE"
@@ -43,10 +40,5 @@ export function On_Message_Found(message) {
 		else {
 			return dispatch(Update_Store(m.type, m.message)) 
 		}
-		// return dispatch({ type: ACTIONS.MESSAGE_FOUND, value: message })
 	} 
-}
-
-export function On_Message(message) {
-  return { type: ACTIONS.MESSAGE_FOUND, value: message }
 }
