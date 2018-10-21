6feba9d0043e6c25d3559d88aa74eb7dbbd1b492
Frederic Sauve Hoover <rsauveho@ualberta.ca>
2018-10-21 14:30:35 -0600

    fixed build script

--- init-ios.sh
+++ init-ios.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 npm install
+mv .babelrc .babelrc2
+react-native-git-upgrade --npm
+mv .babelrc2 .babelrc
 cd ios
+git checkout MockCode.xcodeproj
 pod install
 cd ..
 react-native link
 echo "removing duplicate symbols"
 grep -rl --include \*.h --include \*.cpp --include \*.cc --include \*.in AddLogSink node_modules/react-native/third-party/ | xargs sed -i '' 's/AddLogSink/ReactAddLogSink/g'
 grep -rl --include \*.h --include \*.cpp --include \*.cc --include \*.in RemoveLogSink node_modules/react-native/third-party/ | xargs sed -i '' 's/RemoveLogSink/ReactRemoveLogSink/g'
+echo "done removing duplicate symbols"
